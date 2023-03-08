a669c5effd6b7979d10387f6f6ff249968a4ec0d
Grandi-Luca <luca.grandi9@studio.unibo.it>
2023-03-08 11:21:09 +0100

    generalized folder path

--- client/settings.sh
+++ client/settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FOLDER_SPRING=/home/grandi/project/server/spring #
-PATH_MODEL=${FOLDER_SPRING}/checkpoints/AMR3.parsing.pt
-PATH_AMR_SENTS=/home/grandi/project/graphs
-PATH_DOC_SENTS=/home/grandi/project/documents
+FOLDER_SPRING=../server/spring
+PATH_MODEL=${FOLDER_SPRING}/checkpoints
+PATH_AMR_SENTS=../graphs
+PATH_DOC_SENTS=../documents
 
 if ! [ -d ${PATH_AMR_SENTS} ]
 then
@@ -15,10 +15,16 @@ then
   mkdir ${PATH_DOC_SENTS}
 fi
 
+if ! [ -d ${PATH_MODEL} ]
+then
+  mkdir ${PATH_MODEL}
+fi
+
 python3 -u init_model_inside_server.py \
-  --checkpoint ${PATH_MODEL} \
+  --checkpoint ${PATH_MODEL}/AMR3.parsing.pt \
   --penman-linearization \
   --use-pointer-tokens \
   --amrs-folder ${PATH_AMR_SENTS}
 
-python3 -u test_generation.py
+# run the experiments
+python3 -u test_generation.py ${PATH_AMR_SENTS}
